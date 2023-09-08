bb48fc35a3312c3c1e0f30b83009cd39d5cf1391
mahesh.mulimani <mahesh.mulimani@oneenterprise.com>
2023-09-09 00:42:48 +0530

    second commit

--- src/gDP/Test1.java
+++ src/gDP/Test1.java
@@ -3,13 +3,15 @@ package gDP;
 public class Test1 {
 	public static void main(String[] args) {
 		String hungry = "yes";
-		
-		if(hungry == "yes")
+
+		if (hungry == "yes") {
 			System.out.println("eat something");
+		}
+
 		else {
 			System.out.println("Do work ...");
 		}
-			
+
 	}
 
 }
