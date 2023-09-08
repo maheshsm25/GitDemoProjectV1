b9f2b01eb5bc6c46fc9a078ad526914ba0afe493
mahesh.mulimani <mahesh.mulimani@oneenterprise.com>
2023-09-09 01:56:44 +0530

    Test100.java file

---
+++ src/gDP/Test100.java
@@ -0,0 +1,17 @@
+package gDP;
+
+public class Test100 {
+	public static void main(String[] args) {
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			System.out.println("eat something");
+		}
+
+		else {
+			System.out.println("Do work ...");
+		}
+
+	}
+
+}
