41fb300cc5d7e806bd244e3cae3fc1c0ed6a721f
mahesh.mulimani <mahesh.mulimani@oneenterprise.com>
2023-09-09 02:01:33 +0530

    Commit ...101.java

---
+++ src/gDP/Test101.java
@@ -0,0 +1,17 @@
+package gDP;
+
+public class Test101 {
+	public static void main(String[] args) {
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			System.out.println("eat something");
+		}
+
+		else {
+			System.out.println("Do work ...");
+		}
+
+	}
+
+}
